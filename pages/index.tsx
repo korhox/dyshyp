1e553451c22f4058785ce148bc81be57361e698b
korhox <juuso@korho.fi>
2022-10-16 19:55:42 +0300

    feat: :sparkles: Add prettier configs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,21 @@
-import type { NextPage } from 'next'
-import Hero from '../components/Hero'
-import Textarea from '../components/Textarea'
+import type { NextPage } from "next";
+import Hero from "../components/Hero";
+import Textarea from "../components/Textarea";
 
 const Home: NextPage = () => {
     return (
         <main className="flex-1 flex flex-col">
-            <Hero text={<p>DysHyp makes reading <b>easier</b> <br />and <b>faster</b> for dyslexics.</p>}/>
-            <Textarea/>
+            <Hero
+                text={
+                    <p>
+                        DysHyp makes reading <b>easier</b> <br />
+                        and <b>faster</b> for dyslexics.
+                    </p>
+                }
+            />
+            <Textarea />
         </main>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
