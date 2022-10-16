e34d87ceb435808b764e12ec55e9be96500d1bff
korhox <juuso@korho.fi>
2022-10-16 20:04:01 +0300

    feat: :sparkles: Add font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Script from "next/script";
+import Link from "next/link";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-      <div className="app flex flex-col h-full">
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
-      </div>
-    )
+    return (
+        <>
+            <Head>
+                <title>DysHyp</title>
+                <link rel="stylesheet" type="text/css" href="https://cdn.bittivirta.fi/css/fonts/font-package.css" />
+            </Head>
+            <div className="app flex flex-col h-full">
+                <Header />
+                <Component {...pageProps} />
+                <Footer />
+            </div>
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
