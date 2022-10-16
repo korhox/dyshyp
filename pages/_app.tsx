711e8286123ef46730eb99b6b07bc3fa8594637c
tlitola <touko@litola.net>
2022-10-16 22:34:32 +0300

    style: :art: remove unnecessary import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Script from "next/script";
-import Link from "next/link";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
