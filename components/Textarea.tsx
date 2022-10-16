706c32e84bf97145792d7f0eed14d322adc6806a
korhox <juuso@korho.fi>
2022-10-16 21:21:05 +0300

    feat: :bug: Add awesome bug

--- components/Textarea.tsx
+++ components/Textarea.tsx
@@ -1,6 +1,40 @@
-const Textarea = () => (
-    <div className="container flex-1 flex flex-col p-1">
-        <textarea className="shadow-lg w-full p-5 flex-1" id="editor"></textarea>
-    </div>
-)
+import React from "react";
+import { debounce } from "lodash";
+import { hyphenateHTML } from "hyphen/en";
+
+const divitor = "%%%%%";
+
+const Textarea = () => {
+    const [text, setText] = React.useState<string[]>([]);
+    const debouncedHyphenation = React.useCallback(
+        debounce((newText: string) => {
+            hyphenateHTML(newText, { minWordLength: 3, hyphenChar: divitor }).then((val) => {
+                setText(val.split(divitor));
+            });
+        }, 1000),
+        []
+    );
+    const updateText = (newText: string) => {
+        console.log(newText);
+        setText([newText]);
+        if (newText.length !== 0) {
+            debouncedHyphenation(newText);
+        }
+    };
+    return (
+        <div className="container flex-1 flex flex-col">
+            <section
+                className="shadow-lg w-full p-5 flex-1"
+                id="editor"
+                contentEditable="true"
+                onInput={(e) => {
+                    //@ts-ignore}
+                    updateText(e.target.textContent);
+                }}
+                dangerouslySetInnerHTML={{ __html: `<p>${text.join(" - ")}</p>` }}
+            ></section>
+        </div>
+    );
+};
+
 export default Textarea;
