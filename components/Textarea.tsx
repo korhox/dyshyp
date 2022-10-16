2532827c887186a6091a0735b98e089298a45c26
tlitola <touko@litola.net>
2022-10-17 00:13:32 +0300

    fix: :bug: keep caret at correct position

--- components/Textarea.tsx
+++ components/Textarea.tsx
@@ -2,37 +2,54 @@ import React from "react";
 import { debounce } from "lodash";
 import { hyphenateHTML } from "hyphen/en";
 
-const divisor = "%%%%%";
+const divisor = "";
 
 const Textarea = () => {
-    const [text, setText] = React.useState<string[]>([]);
+    const [text, setText] = React.useState("");
+    const [offset, setOffset] = React.useState<number | undefined>(undefined);
+    const textRef = React.useRef<HTMLParagraphElement>(null);
+
     const debouncedHyphenation = React.useCallback(
         debounce((newText: string) => {
             hyphenateHTML(newText, { minWordLength: 3, hyphenChar: divisor }).then((val) => {
-                setText(val.split(divisor));
+                setText(val);
             });
         }, 1000),
         []
     );
+
+    React.useEffect(() => {
+        if (!offset || !textRef.current) return;
+
+        const range = document.createRange();
+        range.setStart(textRef.current?.childNodes[0], offset);
+
+        const selection = document.getSelection();
+        selection?.removeAllRanges();
+        selection?.addRange(range);
+    }, [text, offset]);
+
     const updateText = (newText: string) => {
+        setText(newText);
+        debouncedHyphenation(newText);
+
         console.log(newText);
-        setText([newText]);
-        if (newText.length !== 0) {
-            debouncedHyphenation(newText);
-        }
+        const range = document.getSelection()?.getRangeAt(0);
+        setOffset(range?.startOffset);
     };
     return (
         <div className="container flex-1 flex flex-col">
-            <section
+            <p
                 className="shadow-lg w-full p-5 flex-1"
                 id="editor"
-                contentEditable="true"
+                contentEditable
                 onInput={(e) => {
-                    //@ts-ignore}
-                    updateText(e.target.textContent);
+                    updateText((e.target as HTMLParagraphElement).innerHTML);
                 }}
-                dangerouslySetInnerHTML={{ __html: `<p>${text.join(" - ")}</p>` }}
-            ></section>
+                suppressContentEditableWarning
+                ref={textRef}
+                dangerouslySetInnerHTML={{ __html: text }}
+            ></p>
         </div>
     );
 };
