403c94866a8213c1596fc52541f2ba6ff8d9105e
tlitola <touko@litola.net>
2022-10-17 20:45:21 +0300

    feat: :construction: Keep the caret always in correct position
    Keep the caret in correct position even if there is html elements under the root

--- components/Textarea.tsx
+++ components/Textarea.tsx
@@ -19,6 +19,24 @@ const flattenChildTextNodes = (node: Node): Node[] => {
     return textNodes;
 };
 
+const getCaretPosition = (root_node: Node, offset_from_root: number): { start_node: Node; offset_from_node: number } => {
+    //Get only text nodes from the children
+    const nodes = flattenChildTextNodes(root_node);
+
+    //Find which node and with which offset the offset_from_root falls into
+    let index = 0;
+    let new_offset = offset_from_root;
+    while (new_offset > nodes[index].length) {
+        new_offset -= nodes[index].length;
+        index += 1;
+    }
+
+    return {
+        start_node: nodes[index],
+        offset_from_node: new_offset,
+    };
+};
+
 const Textarea = () => {
     const [text, setText] = React.useState("");
     const [offset, setOffset] = React.useState<number | undefined>(undefined);
@@ -36,26 +54,28 @@ const Textarea = () => {
     React.useEffect(() => {
         if (!offset || !textRef.current) return;
 
-        console.log(flattenChildTextNodes(textRef.current));
+        //Get the carets position from the offset from start
+        const { start_node, offset_from_node } = getCaretPosition(textRef.current, offset);
 
-        /*const range = document.createRange();
-        range.setStart(textRef.current?.childNodes[0], offset);
+        //Create range representing caret position
+        const range = document.createRange();
+        range.setStart(start_node, offset_from_node);
 
+        //Update caret position
         const selection = document.getSelection();
         selection?.removeAllRanges();
-        selection?.addRange(range);*/
-    }, [text, offset]);
+        selection?.addRange(range);
+    }, [text]);
 
     const updateText = (newText: string) => {
         setText(newText);
         debouncedHyphenation(newText);
 
+        //Calculate the distance the caret is from the start of the editable div and save it as offset.
         const range = window.getSelection()?.getRangeAt(0);
-
         if (!textRef.current || !range) return;
 
         const preCaretRange = range.cloneRange();
-
         preCaretRange.selectNodeContents(textRef.current);
         preCaretRange.setEnd(range?.endContainer, range?.endOffset);
         setOffset(preCaretRange.toString().length);
